d2de94994cbe2c7baa5095ffc3a9646a188425ca
sanghyeokahn <jooa7878@naver.com>
2022-05-22 17:43:53 +0900

    svg animation, remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -13,14 +8,6 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: #fff;
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const Svg = styled.svg`
@@ -50,22 +37,6 @@ const svg = {
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-600, 600], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-600, 0, 600],
-    [
-      "linear-gradient(135deg,rgb(0,210,238),rgb(0,83,238))",
-      "linear-gradient(135deg,rgb(238, 0, 153),rgb(221, 0, 238))",
-      "linear-gradient(135deg,rgb(0, 237, 155),rgb(238, 175, 0))",
-    ]
-  );
-
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
     <Wrapper>
       <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
